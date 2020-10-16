eccc5b1a5d1923c735de282361d6d668b5a98f78
Asmar <asmarcd@gmail.com>
2020-10-15 18:23:04 -0700

    Controller routes done

---
+++ controllers/burgerControllers.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+const burger = require("../model/burger.js");
+
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        let hbsObject = {
+            bugers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.create([
+        "name", "eaten"
+    ], [req.body.name, req.body.eaten], (result) => {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    let condition = "id = " + req.params.id;
+
+    burger.update({
+        eaten: req.body.eaten
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end()
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+    let condition = "id = " + req.params.id;
+
+    burger.delete(condition, (result) => {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
